067c0bf5dfd76b6436b4d41d792e98f67ce090a1
BFetler <bfetler.gmail.com>
2016-02-07 15:47:00 -0500

    add logistic formula for xray direct params

--- formulas.py
+++ formulas.py
@@ -5,30 +5,46 @@
 
 import numpy as np
 
-def getXrayOutcome(gender, ddensity, ob_diam, app_shape):
-#   ignore gender for now
 #   Normal, Mild, Moderate, Severe
+def getXrayOutcomeV1(gender, ddensity, ob_diam, app_shape):
+#   ignore gender for now
     out = 'Normal'
     if (ddensity > 1):
-        out = 'Moderate to Severe'
+        out = 'Moderate'
         if (ob_diam > 7.0):
             out = 'Severe'
         if (app_shape > 1):
-            out = 'Severe2'
+            out = 'Severe'
     return out
 
-# possible to set string outcome from percent?
+# set string outcome from percent
+# Normal < 0.5 < Moderate < 0.69 < Severe
+def getXrayOutcome(gender, ddensity, ob_diam, app_shape):
+    p = getXrayOutcomePercent(gender, ddensity, ob_diam, app_shape)
+    if (p > 0.69):
+        out = 'Severe'
+    elif (p > 0.5):
+        out = 'Moderate'
+    else:
+        out = 'Normal'
+    return out
 
-# calc percent using logistic functions of params?
+# heuristic parameters
+xray_direct_params = { 'double_density':   1.0,
+                       'oblique_diameter': 0.1,
+                       'appendage_shape':  0.2,
+                       'intercept':        0.0 }
+
+# calc percent using logistic functions of params
 def getXrayOutcomePercent(gender, ddensity, ob_diam, app_shape):
-    return 50.0
-
-# dummy func example
-def logistic_fn(loanAmount, fico, params):
-    a1 = -params['FICO.Score']
-    a2 = -params['Amount.Requested']
-    b  = -params['Intercept']
-    p  = 1 / (1 + np.exp( b + a1 * fico + a2 * loanAmount ))
+    a1 = -xray_direct_params['double_density']
+    a2 = -xray_direct_params['oblique_diameter']
+    a3 = -xray_direct_params['appendage_shape']
+    b  = -xray_direct_params['intercept']
+    p  = 1 / (1 + np.exp( b + a1 * (ddensity - 1.0) + \
+           a2 * (ob_diam - 7.0) + a3 * (app_shape - 1.0) ))
+#   print '  a1 %g, a2 %g, a3 %g, b %g' % (a1, a2, a3, b)
+#   print '  ddensity %g, ob_diam %g, app_shape %g => percent %g' % (ddensity, ob_diam, app_shape, p)
     return p
 
 
--- tst_formulas.py
+++ tst_formulas.py
@@ -1,5 +1,5 @@
 import os
-from formulas import getXrayOutcome
+from formulas import getXrayOutcome, getXrayOutcomePercent
 import unittest
 
 class FormulasTestCase(unittest.TestCase):
@@ -11,15 +11,37 @@ class FormulasTestCase(unittest.TestCase):
         assert 'Normal' == getXrayOutcome('male',   0, 5.5, 2)
 
     def test_xrayOutcome_moderate(self):
-        assert 'Moderate to Severe' == getXrayOutcome('female', 2, 5.5, 0)
-        assert 'Moderate to Severe' == getXrayOutcome('male',   2, 7.0, 0)
-        assert 'Moderate to Severe' == getXrayOutcome('female', 2, 5.5, 1)
+        assert 'Moderate' == getXrayOutcome('female', 2, 5.5, 0)
+        assert 'Moderate' == getXrayOutcome('male',   2, 7.0, 0)
 
     def test_xrayOutcome_severe(self):
-        assert 'Severe'  == getXrayOutcome('female', 2, 7.1, 0)
-        assert 'Severe'  == getXrayOutcome('male',   2, 8.1, 1)
-        assert 'Severe2' == getXrayOutcome('male',   2, 8.1, 2)
-        assert 'Severe2' == getXrayOutcome('female', 2, 5.5, 2)
+        assert 'Severe' == getXrayOutcome('female', 2, 5.5, 1)
+        assert 'Severe' == getXrayOutcome('female', 2, 7.1, 0)
+        assert 'Severe' == getXrayOutcome('male',   2, 8.1, 1)
+        assert 'Severe' == getXrayOutcome('male',   2, 8.1, 2)
+        assert 'Severe' == getXrayOutcome('female', 2, 5.5, 2)
+
+    def test_xrayOutcomePercent_normal(self):
+        assert 0.5 >= getXrayOutcomePercent('female', 0, 5.5, 0)  # Normal
+        assert 0.5 >= getXrayOutcomePercent('female', 0, 7.5, 0)  # Normal
+        assert 0.5 >= getXrayOutcomePercent('male',   0, 9.5, 0)  # Normal
+        assert 0.5 >= getXrayOutcomePercent('female', 0, 7.0, 1)  # Normal
+        assert 0.5 >= getXrayOutcomePercent('female', 0, 7.0, 2)  # Normal
+        assert 0.5 >= getXrayOutcomePercent('male',   1, 7.0, 0)  # Normal
+        assert 0.5 >= getXrayOutcomePercent('male',   1, 8.0, 0)  # Normal
+        assert 0.5 >= getXrayOutcomePercent('male',   1, 9.0, 0)  # Normal
+
+    def test_xrayOutcomePercent_moderate(self):
+        assert 0.69 >= getXrayOutcomePercent('male',   2, 5.0, 0)  # Moderate
+        assert 0.69 >= getXrayOutcomePercent('male',   2, 7.0, 0)  # Moderate
+
+    def test_xrayOutcomePercent_severe(self):
+        assert 0.69 < getXrayOutcomePercent('male',   2, 7.2, 0)  # Severe
+        assert 0.69 < getXrayOutcomePercent('male',   2, 8.0, 0)  # Severe
+        assert 0.69 < getXrayOutcomePercent('male',   2, 9.0, 0)  # Severe
+        assert 0.69 < getXrayOutcomePercent('male',   2, 7.0, 2)  # Severe
+        assert 0.69 < getXrayOutcomePercent('male',   2, 8.1, 1)  # Severe
+        assert 0.69 < getXrayOutcomePercent('female', 2, 5.5, 2)  # Severe
 
 if __name__ == '__main__':
     unittest.main()
