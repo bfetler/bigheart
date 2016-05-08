20e0de44494bff6bfc08dc4b5f3dad814eafb760
BFetler <bfetler.gmail.com>
2016-05-07 21:49:37 -0400

    refactor tests

--- tst_formulas.py
+++ tst_formulas.py
@@ -5,43 +5,43 @@ import unittest
 class FormulasTestCase(unittest.TestCase):
 
     def test_xrayOutcome_normal(self):
-        assert 'Normal' == getXrayOutcome('female', 0, 5.5, 0)
-        assert 'Normal' == getXrayOutcome('male',   0, 9.5, 0)
-        assert 'Normal' == getXrayOutcome('female', 0, 5.5, 1)
-        assert 'Normal' == getXrayOutcome('male',   0, 5.5, 2)
+        self.assertEqual('Normal', getXrayOutcome('female', 0, 5.5, 0))
+        self.assertEqual('Normal', getXrayOutcome('male',   0, 9.5, 0))
+        self.assertEqual('Normal', getXrayOutcome('female', 0, 5.5, 1))
+        self.assertEqual('Normal', getXrayOutcome('male',   0, 5.5, 2))
 
     def test_xrayOutcome_moderate(self):
-        assert 'Moderate' == getXrayOutcome('female', 2, 5.5, 0)
-        assert 'Moderate' == getXrayOutcome('male',   2, 7.0, 0)
+        self.assertEqual('Moderate', getXrayOutcome('female', 2, 5.5, 0))
+        self.assertEqual('Moderate', getXrayOutcome('male',   2, 7.0, 0))
 
     def test_xrayOutcome_severe(self):
-        assert 'Severe' == getXrayOutcome('female', 2, 5.5, 1)
-        assert 'Severe' == getXrayOutcome('female', 2, 7.1, 0)
-        assert 'Severe' == getXrayOutcome('male',   2, 8.1, 1)
-        assert 'Severe' == getXrayOutcome('male',   2, 8.1, 2)
-        assert 'Severe' == getXrayOutcome('female', 2, 5.5, 2)
+        self.assertEqual('Severe', getXrayOutcome('female', 2, 5.5, 1))
+        self.assertEqual('Severe', getXrayOutcome('female', 2, 7.1, 0))
+        self.assertEqual('Severe', getXrayOutcome('male',   2, 8.1, 1))
+        self.assertEqual('Severe', getXrayOutcome('male',   2, 8.1, 2))
+        self.assertEqual('Severe', getXrayOutcome('female', 2, 5.5, 2))
 
     def test_xrayOutcomePercent_normal(self):
-        assert 0.5 >= getXrayOutcomePercent('female', 0, 5.5, 0)  # Normal
-        assert 0.5 >= getXrayOutcomePercent('female', 0, 7.5, 0)  # Normal
-        assert 0.5 >= getXrayOutcomePercent('male',   0, 9.5, 0)  # Normal
-        assert 0.5 >= getXrayOutcomePercent('female', 0, 7.0, 1)  # Normal
-        assert 0.5 >= getXrayOutcomePercent('female', 0, 7.0, 2)  # Normal
-        assert 0.5 >= getXrayOutcomePercent('male',   1, 7.0, 0)  # Normal
-        assert 0.5 >= getXrayOutcomePercent('male',   1, 8.0, 0)  # Normal
-        assert 0.5 >= getXrayOutcomePercent('male',   1, 9.0, 0)  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('female', 0, 5.5, 0))  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('female', 0, 7.5, 0))  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('male',   0, 9.5, 0))  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('female', 0, 7.0, 1))  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('female', 0, 7.0, 2))  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('male',   1, 7.0, 0))  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('male',   1, 8.0, 0))  # Normal
+        self.assertGreaterEqual(0.5, getXrayOutcomePercent('male',   1, 9.0, 0))  # Normal
 
     def test_xrayOutcomePercent_moderate(self):
-        assert 0.69 >= getXrayOutcomePercent('male',   2, 5.0, 0)  # Moderate
-        assert 0.69 >= getXrayOutcomePercent('male',   2, 7.0, 0)  # Moderate
+        self.assertGreaterEqual(0.69, getXrayOutcomePercent('male',   2, 5.0, 0))  # Moderate
+        self.assertGreaterEqual(0.69, getXrayOutcomePercent('male',   2, 7.0, 0))  # Moderate
 
     def test_xrayOutcomePercent_severe(self):
-        assert 0.69 < getXrayOutcomePercent('male',   2, 7.2, 0)  # Severe
-        assert 0.69 < getXrayOutcomePercent('male',   2, 8.0, 0)  # Severe
-        assert 0.69 < getXrayOutcomePercent('male',   2, 9.0, 0)  # Severe
-        assert 0.69 < getXrayOutcomePercent('male',   2, 7.0, 2)  # Severe
-        assert 0.69 < getXrayOutcomePercent('male',   2, 8.1, 1)  # Severe
-        assert 0.69 < getXrayOutcomePercent('female', 2, 5.5, 2)  # Severe
+        self.assertLess(0.69, getXrayOutcomePercent('male',   2, 7.2, 0))  # Severe
+        self.assertLess(0.69, getXrayOutcomePercent('male',   2, 8.0, 0))  # Severe
+        self.assertLess(0.69, getXrayOutcomePercent('male',   2, 9.0, 0))  # Severe
+        self.assertLess(0.69, getXrayOutcomePercent('male',   2, 7.0, 2))  # Severe
+        self.assertLess(0.69, getXrayOutcomePercent('male',   2, 8.1, 1))  # Severe
+        self.assertLess(0.69, getXrayOutcomePercent('female', 2, 5.5, 2))  # Severe
 
 if __name__ == '__main__':
     unittest.main()
--- tst_helpers.py
+++ tst_helpers.py
@@ -7,39 +7,39 @@ import unittest
 class HelpersTestCase(unittest.TestCase):
 
     def test_int2bool(self):
-        assert 'True'  == int2bool(1)
-        assert 'False' == int2bool(0)
-        assert 'False' == int2bool(-1)
+        self.assertEqual('True', int2bool(1))
+        self.assertEqual('False', int2bool(0))
+        self.assertEqual('False', int2bool(-1))
 
     def test_bool2int(self):
-        assert 1 == bool2int('True')
-        assert 0 == bool2int('False')
-        assert 0 == bool2int('')
+        self.assertEqual(1, bool2int('True'))
+        self.assertEqual(0, bool2int('False'))
+        self.assertEqual(0, bool2int(''))
 
     def test_int2choice(self):
-        assert 'True'   == int2choice(2)
-        assert 'False'  == int2choice(0)
-        assert 'Unsure' == int2choice(1)
-        assert 'Unsure' == int2choice(-1)
+        self.assertEqual('True', int2choice(2))
+        self.assertEqual('False', int2choice(0))
+        self.assertEqual('Unsure', int2choice(1))
+        self.assertEqual('Unsure', int2choice(-1))
 
     def test_choice2int(self):
-        assert 2 == choice2int('True')
-        assert 0 == choice2int('False')
-        assert 1 == choice2int('Unsure')
-        assert 1 == choice2int('')
+        self.assertEqual(2, choice2int('True'))
+        self.assertEqual(0, choice2int('False'))
+        self.assertEqual(1, choice2int('Unsure'))
+        self.assertEqual(1, choice2int(''))
 
     def test_appendage2choice(self):
-        assert 'Concave or Flat' == appendage2choice(0)
-        assert 'Convex' == appendage2choice(2)
-        assert 'Unsure' == appendage2choice(1)
-        assert 'Unsure' == appendage2choice(-1)
+        self.assertEqual('Concave or Flat', appendage2choice(0))
+        self.assertEqual('Convex', appendage2choice(2))
+        self.assertEqual('Unsure', appendage2choice(1))
+        self.assertEqual('Unsure', appendage2choice(-1))
 
     def test_choice2appendage(self):
-        assert 0 == choice2appendage('Concave')
-        assert 0 == choice2appendage('Flat')
-        assert 2 == choice2appendage('Convex')
-        assert 1 == choice2appendage('Unsure')
-        assert 1 == choice2appendage('')
+        self.assertEqual(0, choice2appendage('Concave'))
+        self.assertEqual(0, choice2appendage('Flat'))
+        self.assertEqual(2, choice2appendage('Convex'))
+        self.assertEqual(1, choice2appendage('Unsure'))
+        self.assertEqual(1, choice2appendage(''))
 
 if __name__ == '__main__':
     unittest.main()
